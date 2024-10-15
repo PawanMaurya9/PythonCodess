ca4d267141eebc7c39d1b7d31cb8fe4d79ca217d
Pawan Maurya <103451625+PawanMaurya9@users.noreply.github.com>
2024-10-15 12:08:49 +0530

    Add files via upload

--- 01.py
+++ 01.py
@@ -1,5 +1,19 @@
-def add():
-    a=10
-    b=10
-    print(a+b)
-add()
+'''try:
+    print('Statement  1')
+    print(10/0)
+    print('Statement  2')
+
+except ZeroDivisionError as msg:
+    print('from except block',type(msg))
+print('Outside try except block')'''
+
+'''try:
+    a = int(input('ENter a: '))
+    b = int(input('Enter b: '))
+    print(a/b)
+    print('hi')
+except ZeroDivisionError as m:
+    print('from except block',type(m))
+except ValueError as v:
+    print('from except block',type(v))
+print('Outside try block')'''
